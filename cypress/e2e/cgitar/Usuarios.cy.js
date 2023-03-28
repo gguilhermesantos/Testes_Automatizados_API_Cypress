326e249b7ed3aea023962875b262eed83499afb1
gguilhermesantos <guimetss@gmail.com>
2023-03-28 15:21:31 -0300

    feat:mudando teste de usuario de pasta

--- cypress/e2e/cgitar/Usuarios.cy.js
+++ cypress/e2e/cgitar/Usuarios.cy.js
@@ -1,15 +1,99 @@
-it('Teste para CriarUser SemDado' , () => {
-    
+it('Teste para criar user com email errado' , () => {
+    const dadosEmailErrado = {
+        "usuarioNomeCompleto": dados.usuarioNomeCompleto,
+        "usuarioEmail": dados.usuarioEmailErrado,
+        "usuarioSenha": dados.usuarioSenha,
+        "usuarioTelefone": dados.usuarioTelefone
+      }
+
+    cy
+    .request({
+        method: 'POST',
+        url: 'usuarios',
+        body: dadosEmailErrado,
+        header: 'Content-Type: application/json',
+        failOnStatusCode: false
+    }).then(function(response){
+        expect(response.body).have.property('message');
+        expect(response.body).have.property('error');
+        expect(response.body).have.property('data');
+        expect(response.status).to.equal(422);
+        expect(response.body.error).to.equal(dados.msgerroemail)
+        //cy.log(JSON.stringify(response.body))
+    })
 });
 
-it('Teste para CriarUser dadoErrado' , () => {
-    
+it('Teste para criar user dado invalido' , () => {
+    const dadosSenhaErrada ={
+        "usuarioNomeCompleto": dados.usuarioNomeCompleto,
+        "usuarioEmail": dados.usuarioEmail,
+        "usuarioSenha": dados.usuarioSenhaErrada,
+        "usuarioTelefone": dados.usuarioTelefone
+      }
+
+      cy
+      .request({
+          method: 'POST',
+          url: 'usuarios',
+          body: dadosSenhaErrada,
+          header: 'Content-Type: application/json',
+          failOnStatusCode: false
+      }).then(function(response){
+          expect(response.body).have.property('message');
+          expect(response.body).have.property('error');
+          expect(response.body).have.property('data');
+          expect(response.status).to.equal(400);
+          expect(response.body.error).to.equal(dados.msgerrosenha)
+          //cy.log(JSON.stringify(response.body))
+      })
 });
 
-it('Teste para CriarUser dadoCorreto' , () => {
-    
+it('Teste para criar user corretamente' , () => {
+    const dadoscorretos ={
+        "usuarioNomeCompleto": dados.usuarioNomeCompleto,
+        "usuarioEmail": dados.usuarioEmailNovo,
+        "usuarioSenha": dados.usuarioSenhaNovo,
+        "usuarioTelefone": dados.usuarioTelefone
+      }
+
+      cy
+      .request({
+          method: 'POST',
+          url: 'usuarios',
+          body: dadoscorretos,
+          header: 'Content-Type: application/json',
+          failOnStatusCode: false
+      }).then(function(response){
+          expect(response.body).have.property('message');
+          expect(response.body).have.property('error');
+          expect(response.body).have.property('data');
+          expect(response.status).to.equal(409);
+          expect(response.body.error).to.equal(dados.msguserexiste)
+          cy.log(JSON.stringify(response.body))
+      })
 });
 
-it('Teste para CriarUser dadoJaCriado' , () => {
-    
+it('Teste para criar user com dado ja criado' , () => {
+    const dadoscorretos ={
+        "usuarioNomeCompleto": dados.usuarioNomeCompleto,
+        "usuarioEmail": dados.usuarioEmail,
+        "usuarioSenha": dados.usuarioSenha,
+        "usuarioTelefone": dados.usuarioTelefone
+      }
+
+      cy
+      .request({
+          method: 'POST',
+          url: 'usuarios',
+          body: dadoscorretos,
+          header: 'Content-Type: application/json',
+          failOnStatusCode: false
+      }).then(function(response){
+          expect(response.body).have.property('message');
+          expect(response.body).have.property('error');
+          expect(response.body).have.property('data');
+          expect(response.status).to.equal(409);
+          expect(response.body.error).to.equal(dados.msguserexiste)
+          cy.log(JSON.stringify(response.body))
+      })
 });
