73e92b646c937358093d37a32f8d3fc783dbbbaf
gguilhermesantos <guimetss@gmail.com>
2023-03-28 15:12:04 -0300

    feat:Criação de teste de usuario

--- cypress/e2e/cgitar/Autenticacao.cy.js
+++ cypress/e2e/cgitar/Autenticacao.cy.js
@@ -1,21 +1,76 @@
+const dados = require('../../fixtures/dados.json')
+
 it('Teste com email errado', () => {
-    cy.request('GET','api/deck/new/shuffle/?deck_count=1');
-})
+    const dadosEmailErrado = {
+        "usuarioNomeCompleto": dados.usuarioNomeCompleto,
+        "usuarioEmail": dados.usuarioEmailErrado,
+        "usuarioSenha": dados.usuarioSenha,
+        "usuarioTelefone": dados.usuarioTelefone
+      }
+
+    cy
+    .request({
+        method: 'POST',
+        url: 'usuarios',
+        body: dadosEmailErrado,
+        header: 'Content-Type: application/json',
+        failOnStatusCode: false
+    }).then(function(response){
+        expect(response.body).have.property('message');
+        expect(response.body).have.property('error');
+        expect(response.body).have.property('data');
+        expect(response.status).to.equal(422);
+        expect(response.body.error).to.equal(dados.msgerroemail)
+        //cy.log(JSON.stringify(response.body))
+    })
+});
 
 it('Teste com senha errada',function(){
-    cy.request({
-        method: 'GET',
-        url: 'api/deck/new/shuffle/?deck_count=1',
-        }).then(function(response){
-            expect(response.body).have.property('success');
-            expect(response.body).have.property('deck_id');
-            expect(response.body).have.property('remaining');
-            expect(response.body).have.property('shuffled');
-            expect(response.status).to.equal(200);
-            cy.log(JSON.stringify(response.body))
-        });
+    const dadosSenhaErrada ={
+        "usuarioNomeCompleto": dados.usuarioNomeCompleto,
+        "usuarioEmail": dados.usuarioEmail,
+        "usuarioSenha": dados.usuarioSenhaErrada,
+        "usuarioTelefone": dados.usuarioTelefone
+      }
+
+      cy
+      .request({
+          method: 'POST',
+          url: 'usuarios',
+          body: dadosSenhaErrada,
+          header: 'Content-Type: application/json',
+          failOnStatusCode: false
+      }).then(function(response){
+          expect(response.body).have.property('message');
+          expect(response.body).have.property('error');
+          expect(response.body).have.property('data');
+          expect(response.status).to.equal(400);
+          expect(response.body.error).to.equal(dados.msgerrosenha)
+          //cy.log(JSON.stringify(response.body))
+      })
 });
 
 it('Teste de Autenticação', function(){
+    const dadoscorretos ={
+        "usuarioNomeCompleto": dados.usuarioNomeCompleto,
+        "usuarioEmail": dados.usuarioEmail,
+        "usuarioSenha": dados.usuarioSenha,
+        "usuarioTelefone": dados.usuarioTelefone
+      }
 
+      cy
+      .request({
+          method: 'POST',
+          url: 'usuarios',
+          body: dadoscorretos,
+          header: 'Content-Type: application/json',
+          failOnStatusCode: false
+      }).then(function(response){
+          expect(response.body).have.property('message');
+          expect(response.body).have.property('error');
+          expect(response.body).have.property('data');
+          expect(response.status).to.equal(409);
+          expect(response.body.error).to.equal(dados.msguserexiste)
+          cy.log(JSON.stringify(response.body))
+      })
 });
